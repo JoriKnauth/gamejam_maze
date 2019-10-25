2912ed9dbac63cbe3b9b8636e0af62cf15d358ad
DudeStranger <strangergamesm.k@gmail.com>
2019-10-25 12:43:02 +0200

    Initial commit

---
+++ Assets/Scripts/Char_.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char_ : MonoBehaviour
+{
+    public float raylenght;
+    public float tilelenght;
+    public Transform thistransform;
+
+    public void Awake() 
+    {
+        thistransform = transform;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) 
+        {
+            thistransform.position = new Vector3(thistransform.position.x + tilelenght, thistransform.position.y, thistransform.position.z);
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            thistransform.position = new Vector3(thistransform.position.x + tilelenght, thistransform.position.y, thistransform.position.z);
+        } else if (Input.GetKeyDown(KeyCode.A)) {
+            thistransform.position = new Vector3(thistransform.position.x + tilelenght, thistransform.position.y, thistransform.position.z);
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            thistransform.position = new Vector3(thistransform.position.x + tilelenght, thistransform.position.y, thistransform.position.z);
+        }
+
+    }
+
+    private void Move() 
+        {
+
+    }
+}
