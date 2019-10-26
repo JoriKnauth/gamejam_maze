17e8fcc1af23b31f2ce42c7496b2658db8384900
Juri Knauth <knauth.juri@gmail.com>
2019-10-26 14:19:23 +0200

    TriggerDoor ANim

--- Assets/Scripts/TriggerableDoor.cs
+++ Assets/Scripts/TriggerableDoor.cs
@@ -1,15 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class TriggerableDoor : Triggerable
 {
     [SerializeField] private GameObject thisGameObject;
+
+    public Transform thisTransform;
+    public Transform playerTransform;
+
+    public float moveDuration;
+
+    public Vector3 up;
+    public Vector3 down;
+
+    public Ease easeType;
+
+    public Tween moveTween;
 
     public override void Triggered()
     {
         base.Triggered();
         isTriggered = !isTriggered;
         thisGameObject.SetActive(isTriggered);
+
+        SetMoveOut(isTriggered ? up : down);
+    }
+
+    private void SetMoveOut(Vector3 position)
+    {
+        DOTween.Kill(moveTween);
+
+        moveTween = thisTransform.DOMove(position, moveDuration, false).SetEase(easeType);
     }
 }
