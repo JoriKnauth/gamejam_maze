7a6180ba55c4e0f118ac2030214005a26315286f
Juri <knauth.juri@gmail.com>
2020-01-19 13:52:37 +0100

    Editor version 2019.2.18f1

---
+++ Assets/Scripts/Editor/CleanRotations.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CleanRotations : ScriptableWizard
+{
+    public Transform[] Transforms;
+
+    public bool cleanRotationX = true;
+    public bool cleanRotationY = true;
+    public bool cleanRotationZ = true;
+
+    [MenuItem("Custom/Clean Rotations")]
+
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard("Clean Rotations", typeof(CleanRotations), "Clean Rotations");
+    }
+
+    void OnWizardCreate()
+    {
+        if (Transforms.Length == 0)
+        {
+            return;
+        }
+
+        foreach (Transform thisTransform in Transforms)
+        {
+            Vector3 rot = thisTransform.eulerAngles;
+
+            if (cleanRotationX)
+            {
+                rot.x = CleanRotation(rot.x, thisTransform);
+            }
+            if (cleanRotationY)
+            {
+                rot.y = CleanRotation(rot.y, thisTransform);
+            }
+            if (cleanRotationZ)
+            {
+                rot.z = CleanRotation(rot.z, thisTransform);
+            }
+
+            rot.x = Vector3.Angle(new Vector3(rot.x, 0f, 0f), thisTransform.right);
+            rot.y = Vector3.Angle(new Vector3(0f, rot.y, 0f), thisTransform.up);
+            rot.z = Vector3.Angle(new Vector3(0f, 0f, rot.z), thisTransform.forward);
+
+
+            thisTransform.rotation = Quaternion.identity;
+            thisTransform.eulerAngles = rot;
+
+            thisTransform.rotation = Quaternion.Euler(rot);
+        }
+    }
+
+    private float CleanRotation(float _value, Transform thisTransform)
+    {
+        float newValue = _value;
+
+        newValue %= 360f;
+
+        if (newValue < 0f)
+        {
+            newValue += 360f;
+        }
+
+
+        Debug.Log("old Value " + _value + " new Value " + newValue);
+
+        return newValue;
+    }
+}
---
+++ Assets/Scripts/Editor/RoundTransform.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class RoundTransform : ScriptableWizard
+{
+    public Transform[] Transforms;
+
+    public float roundPositionTo = 0.5f;
+    private float roundPositionToValue;
+    private bool roundPositionBiggerThanOne;
+    public bool roundPositionX = true;
+    public bool roundPositionY = true;
+    public bool roundPositionZ = true;
+
+    public float roundRotationTo = 90f;
+    private float roundRotationToValue;
+    private bool roundRotationBiggerThanOne;
+    public bool roundRotationX = true;
+    public bool roundRotationY = true;
+    public bool roundRotationZ = true;
+
+    [MenuItem("Custom/Round Transforms")]
+
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard("Round Transforms", typeof(RoundTransform), "Round Transforms");
+    }
+
+    void OnWizardCreate()
+    {
+        if (Transforms.Length == 0)
+        {
+            return;
+        }
+
+        if (roundPositionTo < 1)
+        {
+            if (roundPositionTo == 0)
+            {
+                return;
+            }
+            roundPositionBiggerThanOne = false;
+            roundPositionToValue = 1f / roundPositionTo;
+        }
+        else
+        {
+            roundPositionBiggerThanOne = true;
+            roundPositionToValue = roundPositionTo;
+        }
+
+        if (roundRotationTo < 1)
+        {
+            if (roundRotationTo == 0)
+            {
+                return;
+            }
+            roundRotationBiggerThanOne = false;
+            roundRotationToValue = 1f / roundRotationTo;
+        }
+        else
+        {
+            roundRotationBiggerThanOne = true;
+            roundRotationToValue = roundRotationTo;
+        }
+
+
+
+        foreach (Transform thisTransform in Transforms)
+        {
+            Vector3 pos = thisTransform.position;
+
+            if (roundPositionX)
+            {
+                pos.x = roundPositionBiggerThanOne ? RoundToWholeNumbers(pos.x, roundPositionToValue) : RoundToFraction(pos.x, roundPositionToValue);
+            }
+            if (roundPositionY)
+            {
+                pos.y = roundPositionBiggerThanOne ? RoundToWholeNumbers(pos.y, roundPositionToValue) : RoundToFraction(pos.y, roundPositionToValue);
+            }
+            if (roundPositionZ)
+            {
+                pos.z = roundPositionBiggerThanOne ? RoundToWholeNumbers(pos.z, roundPositionToValue) : RoundToFraction(pos.z, roundPositionToValue);
+            }
+
+
+            Vector3 rot = thisTransform.eulerAngles;
+
+            if (roundRotationX)
+            {
+                rot.x = roundRotationBiggerThanOne ? RoundToWholeNumbers(rot.x, roundRotationToValue) : RoundToFraction(rot.x, roundRotationToValue);
+            }
+            if (roundRotationY)
+            {
+                rot.y = roundRotationBiggerThanOne ? RoundToWholeNumbers(rot.y, roundRotationToValue) : RoundToFraction(rot.y, roundRotationToValue);
+            }
+            if (roundRotationZ)
+            {
+                rot.z = roundRotationBiggerThanOne ? RoundToWholeNumbers(rot.z, roundRotationToValue) : RoundToFraction(rot.z, roundRotationToValue);
+            }
+
+            thisTransform.eulerAngles = rot;
+            thisTransform.position = pos;
+
+        }
+
+    }
+
+    private float RoundToFraction(float _value, float roundTo)
+    {
+        return Mathf.Round(_value * roundTo) / roundTo;
+    }
+
+    private float RoundToWholeNumbers(float _value, float roundTo)
+    {
+        return Mathf.Round(_value / roundTo) * roundTo;
+    }
+}
